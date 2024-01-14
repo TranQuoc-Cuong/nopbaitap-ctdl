a711ba1982c27705761a66b88a706fa9eaf997d4
TranQuoc-Cuong <156506372+TranQuoc-Cuong@users.noreply.github.com>
2024-01-14 22:45:37 +0700

    add lại file

--- buoi1/chuyenDoiSoThapPhanSangSoNhiPhan.py
+++ buoi1/chuyenDoiSoThapPhanSangSoNhiPhan.py
@@ -1,6 +1,6 @@
 def chuyenDoiSoThapPhanSangNhiPhan(so_thap_phan):
-    if so_thap_phan == 1:
-        print(1, end="")
+    if so_thap_phan in [0,1]:
+        print(so_thap_phan, end="")
     else:
         chuyenDoiSoThapPhanSangNhiPhan(int(so_thap_phan / 2))
         print(so_thap_phan % 2, end="")
