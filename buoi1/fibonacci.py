517b1bed617cb11852080cc006b460d8f351af1f
TranQuoc-Cuong <156506372+TranQuoc-Cuong@users.noreply.github.com>
2024-01-15 14:36:02 +0700

    Sữa nhầm lẫn fibonacci.py

--- buoi1/fibonacci.py
+++ buoi1/fibonacci.py
@@ -1,10 +1,9 @@
 def fibonacci(x):
     assert x >= 0 and int(x) == x
-    # if x in [0, 1]:
-    #     return x
-    # else:
-    #     return fibonacci(x - 1) + fibonacci(x - 2)
-    print(x)
+    if x in [0, 1]:
+        return x
+    else:
+        return fibonacci(x - 1) + fibonacci(x - 2)
     
 x = int(input("nhap n: "))
-fibonacci(x)
+print(int(fibonacci(x)))
